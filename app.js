966ab95244082300bdc86ddb23bf2b924e1c1618
Abdulghaffar39 <abdulghaffar15a@gmail.com>
2025-05-06 17:04:58 +0500

    problem

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
-// disableboxes
-// enableboxes
-// forEach
-// addElement
-// disabled
-// ClassList
-// Remove
-// Hide
-// pattern
 
-
-let container = document.querySelector(".msg_container");
+let msgcontainer = document.querySelector(".msg_container");
 let msg = document.querySelector(".msg");
-let new_btn = document.querySelector(".new_btn");
+let newbtn = document.querySelector(".new_btn");
 
 let boxs = document.querySelector(".box");
 let reset = document.querySelector(".reset_btn");
 
-
 let turnO = true;
 
-console.log(turnO);
-
-let winPetterns = [
+let winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
   [0, 4, 8],
@@ -35,12 +22,11 @@ let winPetterns = [
 const resetGame = () => {
     turnO = true;
     enableboxes();
-    container.classList.add("hide");
+    msgcontainer.classList.add("hide");
 };
 
 
 boxs.forEach((box) => {
-
     box.addEventListener('click', () => {
         if (turnO) {
             box.innerText = 'O';
@@ -51,13 +37,13 @@ boxs.forEach((box) => {
         }
         box.disabled = true;
         checkWin()
-    })
-})
+    });
+});
 
 
 const disabledboxes = () => {
     boxs.forEach((box) => {
-        box.disabled = true
+        box.disabled = true;
     });
 };
 
@@ -72,25 +58,39 @@ const enableboxes = () => {
 
 const showWinner = (winner) => {
     msg.innerHTML = `Player ${winner} wins!`;
-    container.classList.remove("hide");
-    disabledboxes()
+    msgcontainer.classList.remove("hide");
+    disabledboxes();
 }
 
 
 const checkWin = () => {
-    for (let pattern of winPetterns){
-        let pos1val = boxs[pattern[0]].innerHTML;
-        let pos2val = boxs[pattern[1]].innerHTML;
-        let pos3val = boxs[pattern[2]].innerHTML;
+    for (let pattern of winPatterns) {
+      let pos1val = boxs[pattern[0]].innerHTML;
+      let pos2val = boxs[pattern[1]].innerHTML;
+      let pos3val = boxs[pattern[2]].innerHTML;
 
-        if (pos1val !== '' && pos1val === pos2val && pos2val === pos3val) {
-            showWinner(pos1val)
-            return;
-        }
+      if (pos1val !== "" && pos1val === pos2val && pos2val === pos3val) {
+        showWinner(pos1val);
+        return;
+      }
     }
 };
 
-new_btn.addEventListener("click", resetGame);
+newbtn.addEventListener("click", resetGame);
 resetbtn.addEventListener("click", resetGame);
+
+
+
+
+
+// disableboxes
+// enableboxes
+// forEach
+// addElement
+// disabled
+// ClassList
+// Remove
+// Hide
+// pattern
 
 
